2bee310ec214c6bd57a49d67193b5f45f14ed156
Ian-Dzy <zdon070@aucklanduni.ac.nz>
2024-04-12 16:59:47 +1200

    Extract logging logic

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const winston = require("winston");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const app = express();
+
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "info",
-});
 
 // throw new Error("Something failed during startup.");
 
