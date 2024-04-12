ddd003e2b605f455e5f0e1f55e6282e51a06631f
Ian-Dzy <zdon070@aucklanduni.ac.nz>
2024-04-12 17:10:23 +1200

    Extract the validation logic

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
+const winston = require("winston");
 const express = require("express");
 const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-// throw new Error("Something failed during startup.");
-
-// const p = Promise.reject(new Error("Something failed miserably!"));
-// p.then(() => console.log("Done"));
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
